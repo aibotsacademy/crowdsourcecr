a62095ca9165d2e4fa3d92352a331e7ad0482530
aibots <leandrocg10@gmail.com>
2021-10-25 16:32:37 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("01 July 2021 9:56:00 GMT+01:00");			
+		var endTime = new Date("10 November 2021 9:56:00 GMT+01:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
